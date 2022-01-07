95827e8c913a5e2b5970e92e4a3529ebf468a3f2
pyojisung <p990301@gmail.com>
2022-01-07 14:20:30 +0900

    BaekJoon 1330 두수 비교하기

---
+++ src/joon1330.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class joon1330 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+
+        if(a > b)
+            System.out.println(">");
+        else if( a == b)
+            System.out.println("==");
+        else
+            System.out.println("<");
+    }
+}
