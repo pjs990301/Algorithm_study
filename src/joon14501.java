75ff03e5413bf51c2893e28c04396c3712f750d8
pyojisung <p990301@gmail.com>
2022-01-07 14:20:03 +0900

    BaekJoon 14501 퇴사

--- src/joon14501.java
+++ src/joon14501.java
@@ -1,10 +1,30 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class joon14501 {
-    public static void main(String[] args) {
-        int n;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        int[] t = new int[n];
+        int[] p = new int[n];
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(reader.readLine());
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            if (i + t[i] <= n) {
+                dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
+            }
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        }
+        System.out.println(dp[n]);
 
     }
 }
