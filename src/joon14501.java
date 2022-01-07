348b45c2227e51716fc7e3a70a3483af6cd658fd
pyojisung <p990301@gmail.com>
2022-01-07 13:41:41 +0900

    BaekJoon 14501 퇴사

---
+++ src/joon14501.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class joon14501 {
+    public static void main(String[] args) {
+        int n;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
