dab4de86a4b05db15f1ab44dc7d8e29646f1bece
pyojisung <p990301@gmail.com>
2022-01-07 14:43:53 +0900

    BaekJoon 2884 알람 시계

---
+++ src/joon2884.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class joon2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int clock = h * 60 + m - 45;
+        h = clock / 60;
+        m = clock % 60;
+        if (m < 0) {
+            h = 23;
+            m = 60 + m;
+        }
+
+        System.out.println(h + " " + m);
+    }
+}
