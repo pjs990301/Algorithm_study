c89cfc987e25dccdbe37a4b064946ed2e7d5aa42
pyojisung <p990301@gmail.com>
2022-01-07 15:00:01 +0900

    BaekJoon 15552 빠른 A+B

--- src/joon10950.java
+++ src/joon10950.java
@@ -1,21 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.StringTokenizer;
 
 public class joon10950 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        int a, b;
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            System.out.println(a+b);
+            bw.write((Integer.parseInt(st.nextToken())+ Integer.parseInt(st.nextToken()))+ "\n");
         }
-
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
