c3681eb78c838be14bfa500f2e83a9cfa83db68a
pyojisung <p990301@gmail.com>
2022-01-07 15:11:51 +0900

    BaekJoon 11021 A+B - 7

--- src/joon11021.java
+++ src/joon11021.java
@@ -11,7 +11,8 @@ public class joon11021 {
         int n = Integer.parseInt(st.nextToken());
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            bw.write((Integer.parseInt(st.nextToken())+ Integer.parseInt(st.nextToken()))+ "\n");
+            int c = Integer.parseInt(st.nextToken())+ Integer.parseInt(st.nextToken());
+            bw.write("Case #"+i+": "+c+ "\n");
         }
         bw.flush();
         br.close();
