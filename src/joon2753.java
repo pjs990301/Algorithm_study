b822c2099a06f5d37ae76e7233231ce35a1f1e7c
pyojisung <p990301@gmail.com>
2022-01-07 14:26:04 +0900

    BaekJoon 2753 윤년

---
+++ src/joon2753.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class joon2753 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int year = Integer.parseInt(st.nextToken());
+
+        if(year%4==0) {
+            if (year % 400 == 0)
+                System.out.println("1");
+            else if (year % 100 != 0)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+        else
+            System.out.println("0");
+    }
+}
