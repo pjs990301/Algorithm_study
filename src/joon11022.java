48d10f87b2ccf159888ca85bbdbb5836f3619129
pyojisung <p990301@gmail.com>
2022-01-07 15:17:43 +0900

    BaekJoon 11022 A+B - 8

---
+++ src/joon11022.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.StringTokenizer;
+
+public class joon11022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = a + b;
+            bw.write("Case #" + i + ": " + a + " + " + b + " = " + c + "\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
