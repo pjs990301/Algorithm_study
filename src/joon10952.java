9d47c9616986fa8dd47ac79cbdde7e088bf29736
pyojisung <p990301@gmail.com>
2022-01-07 15:35:49 +0900

    BaekJoon 10952 A+B - 5

---
+++ src/joon10952.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class joon10952 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            StringTokenizer str = new StringTokenizer(br.readLine());
+            int num1 = Integer.valueOf(str.nextToken());
+            int num2 = Integer.valueOf(str.nextToken());
+            if(num1 == 0 && num2 == 0) break;
+            int sum = num1 + num2;
+            System.out.println(sum);
+        }
+    }
+}
