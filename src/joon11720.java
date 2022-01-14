738f574687b01924b6fcea422aa7450aa4491c7f
pyojisung <p990301@gmail.com>
2022-01-14 14:15:03 +0900

    BaekJoon 11720 숫자의 합

---
+++ src/joon11720.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class joon11720 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        int sum = 0;
+        n = Integer.parseInt(scanner.nextLine());
+        String str = scanner.nextLine();
+        for (int i = 0; i < n; i++) {
+            sum += str.charAt(i) - '0';
+        }
+        System.out.println(sum);
+    }
+}
