93978228ae6cc4ee8fcde03df74ced4909e49609
pyojisung <p990301@gmail.com>
2022-01-14 14:49:49 +0900

    BaekJoon 11654 아스키 코드

---
+++ src/joon11654.java
@@ -0,0 +1,12 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class joon11654 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String a = in.next();
+        byte[] b = a.getBytes(StandardCharsets.US_ASCII);
+        System.out.println(b[0]);
+    }
+}
