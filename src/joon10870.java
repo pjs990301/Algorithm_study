264026a722b4b11d52881964c20aba3dded02cd7
pyojisung <p990301@gmail.com>
2022-01-14 12:36:07 +0900

    BaekJoon 10870 피보나치 수 5

---
+++ src/joon10870.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class joon10870 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(Fibo(n));
+    }
+
+    public static int Fibo(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return 1;
+        }
+        else
+            return Fibo(n-2)+Fibo(n-1);
+    }
+}
