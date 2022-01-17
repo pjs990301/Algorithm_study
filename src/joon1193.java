2fe45c6f0b29a813bca6a5d73b96ebc45c4a0ff8
pyojisung <p990301@gmail.com>
2022-01-17 15:51:38 +0900

    BaekJoon 1193 분수찾기

---
+++ src/joon1193.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class joon1193 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int cross_count = 1, prev_count_sum = 0;
+
+        while (true) {
+            if (n <= prev_count_sum + cross_count) {
+
+                if (cross_count % 2 == 1) {
+                    System.out.print((cross_count - (n - prev_count_sum - 1)) + "/" + (n - prev_count_sum));
+                    break;
+                } else {
+                    System.out.print((n - prev_count_sum) + "/" + (cross_count - (n - prev_count_sum - 1)));
+                    break;
+                }
+
+            } else {
+                prev_count_sum += cross_count;
+                cross_count++;
+            }
+        }
+    }
+}
