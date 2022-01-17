ef5d92682f0f607dbf84915588ae88d9b7638162
pyojisung <p990301@gmail.com>
2022-01-17 15:04:58 +0900

    BaekJoon 2292 벌집

---
+++ src/joon2292.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class joon2292 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int layer = 1;
+        int num = 0;
+        int totalNum = 1;
+        while (true) {
+            if (n <= totalNum) {
+                break;
+            } else {
+                num += 6;
+                totalNum += num;
+                layer++;
+            }
+        }
+        System.out.println(layer);
+    }
+}
