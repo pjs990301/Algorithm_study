b6c9190c247c84ac7ca11030823d1b154e857df4
pyojisung <p990301@gmail.com>
2022-01-17 14:17:20 +0900

    BaekJoon 1110 더하기 사이클

---
+++ src/joon1110.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class joon1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        int N = Integer.parseInt(br.readLine());
+        int copy = N;
+
+        while (true) {
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+            count++;
+            if(copy == N)
+                break;
+        }
+        System.out.println(count);
+    }
+}
