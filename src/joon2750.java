82cc89bc4f83a6da5eb78631ef9c60afbaa4b75b
pyojisung <p990301@gmail.com>
2022-01-18 14:04:01 +0900

    BaekJoon 2750 수 정렬하기

---
+++ src/joon2750.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class joon2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(in.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(in.readLine());
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++)
+            System.out.println(arr[i]);
+    }
+}
