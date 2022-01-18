f7f1839c5a1885cf273459d8a9927c8f6c9d01c3
pyojisung <p990301@gmail.com>
2022-01-18 13:41:31 +0900

    BaekJoon 2869 달팽이는 올라가고 싶다

---
+++ src/joon2869.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class joon2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(in.readLine());
+        int A = Integer.parseInt(str.nextToken());
+        int B = Integer.parseInt(str.nextToken());
+        int V = Integer.parseInt(str.nextToken());
+        int day = 0;
+
+        day = (V - B) / (A - B);
+
+        if ((V - B) % (A - B) != 0) {
+            day++;
+        }
+        System.out.println(day);
+    }
+}
