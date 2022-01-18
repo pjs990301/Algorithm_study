ed455e2875726356d8ef2517c18f8b097c96ef95
pyojisung <p990301@gmail.com>
2022-01-18 14:22:20 +0900

    BaekJoon 2751 수 정렬하기 2

---
+++ src/joon2751.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class joon2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(list);
+        for(int value : list) {
+            sb.append(value).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
