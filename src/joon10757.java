da40afbcd5adc9c7ff99d8f31268845cf9420dd2
pyojisung <p990301@gmail.com>
2022-01-18 13:53:27 +0900

    BaekJoon 10757 큰 수 A + B

---
+++ src/joon10757.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class joon10757 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(in.readLine());
+        BigInteger A = new BigInteger(str.nextToken());
+        BigInteger B = new BigInteger(str.nextToken());
+
+        System.out.println(A.add(B));
+
+
+    }
+}
