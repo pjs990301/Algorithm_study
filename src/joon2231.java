977424a91c1a0978d2359cf59ba1966615482908
pyojisung <p990301@gmail.com>
2022-01-25 11:45:39 +0900

    BaekJoon 2231 분해합

---
+++ src/joon2231.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class joon2231 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(in.readLine());
+        int N = Integer.parseInt(str.nextToken());
+        int result = 0;
+
+        for(int i = 0; i < N; i++) {
+            int number = i;
+            int sum = 0;
+
+            while(number != 0) {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            if(sum + i == N) {
+                result = i;
+                break;
+            }
+
+        }
+        System.out.println(result);
+    }
+}
