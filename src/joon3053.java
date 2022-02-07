f2a831051db5c5be84f940816cef4e4161bd27e0
pyojisung <p990301@gmail.com>
2022-02-07 14:41:38 +0900

    BaekJoon 3053 택시기하학

---
+++ src/joon3053.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class joon3053 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double r = in.nextDouble();
+        System.out.println(r * r * Math.PI);
+        System.out.println(r * r * 2);
+    }
+}
