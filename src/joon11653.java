1f5edadf86d7695c3fba85429c35e04788195540
pyojisung <p990301@gmail.com>
2022-02-07 16:25:25 +0900

    BaekJoon 11653 소인수분해

---
+++ src/joon11653.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.System.exit;
+
+public class joon11653 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+        if (n == 1) {
+            exit(0);
+        }
+        int i = 2;
+        while(n!=1){
+            if(n%i==0){
+                n = n / i;
+                System.out.println(i);
+            }
+            else {
+                i++;
+            }
+        }
+    }
+}
