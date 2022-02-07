e15a448874a42221b0a09d4a4bf5800cf608087e
pyojisung <p990301@gmail.com>
2022-02-07 11:23:45 +0900

    프로그래머스 2021 카카오 채용연계형 인턴십 숫자 문자열과 영단어

--- src/programmers81301.java
+++ src/programmers81301.java
@@ -1,3 +1,22 @@
-public class programmers81301 {
+import java.util.Scanner;
 
+public class programmers81301 {
+    public static void main(String[] args) {
+        Solution sc = new Solution();
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        System.out.println(sc.solution(input));
+    }
+}
+
+class Solution {
+    public int solution(String s) {
+        String[] arr1 = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] arr2 = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+        for(int i = 0; i < 10; i++){
+            s = s.replace(arr1[i] , arr2[i]);
+        }
+        return Integer.parseInt(s);
+    }
 }
