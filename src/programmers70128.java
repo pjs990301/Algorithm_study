2fd5185f5165d69c48fd4273d2c31a807867d22d
pyojisung <p990301@gmail.com>
2022-02-07 11:27:45 +0900

    프로그래머스 월간코드챌린지 시즌1 내적

---
+++ src/programmers70128.java
@@ -0,0 +1,11 @@
+public class programmers70128 {
+    public static void main(String[] args) {
+
+    }
+}
+class Solution70128 {
+    public int solution(int[] a, int[] b) {
+        int answer = 1234567890;
+        return answer;
+    }
+}
