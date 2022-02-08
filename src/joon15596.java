44796a19aac22bcc0d1c5156b26d1c605df83047
pyojisung <p990301@gmail.com>
2022-02-08 15:24:58 +0900

    BaekJoon 15596 정수 N개의 합(함수만 구현)

---
+++ src/joon15596.java
@@ -0,0 +1,15 @@
+public class joon15596 {
+
+    public static void main(String[] args) {
+
+    }
+
+    long sum(int[] a) {
+        long result = 0;
+        for (int i = 0; i < a.length; i++) {
+            result += a[i];
+        }
+        return result;
+    }
+
+}
