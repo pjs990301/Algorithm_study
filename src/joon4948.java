d6b8b43d80c24ca481ed83c780d2fe3273b9a7a1
pyojisung <p990301@gmail.com>
2022-02-09 14:47:03 +0900

    BaekJoon 4948 베르트랑 공준

---
+++ src/joon4948.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class joon4948 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+            int n = Integer.parseInt(st.nextToken());
+            if (n == 0) {
+                break;
+            }
+            boolean prime[] = new boolean[246913];
+            int count = 0;
+
+            prime[0] = prime[1] = true;
+
+            for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+                if (prime[i]) continue;
+                for (int j = i * i; j < prime.length; j += i) {
+                    prime[j] = true;
+                }
+            }
+            for (int i = n + 1; i <= 2 * n; i++) {
+                if (!prime[i]) count++;
+            }
+            System.out.println(count);
+        }
+    }
+}
