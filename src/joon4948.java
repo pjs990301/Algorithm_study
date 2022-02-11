c5253a97cb7d94b76ff0b4376e212e905c7f5add
pyojisung <p990301@gmail.com>
2022-02-11 14:06:17 +0900

    BaekJoon 4948 베르트랑 공준

--- src/joon4948.java
+++ src/joon4948.java
@@ -16,7 +16,6 @@ public class joon4948 {
             int count = 0;
 
             prime[0] = prime[1] = true;
-
             for (int i = 2; i <= Math.sqrt(prime.length); i++) {
                 if (prime[i]) continue;
                 for (int j = i * i; j < prime.length; j += i) {
