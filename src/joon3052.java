cb8c742d661fb6e2055ba9bde99f1a655cc4c0df
pyojisung <p990301@gmail.com>
2022-02-11 15:26:32 +0900

    BaekJoon 3052 나머지

---
+++ src/joon3052.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class joon3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr1 = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr1[i] = Integer.parseInt(br.readLine()) % 42;
+        }
+        System.out.println(Arrays.stream(arr1).distinct().count());
+
+    }
+}
