8c1eacf2c8295aed3faf9e3611b0ae7addadcbde
pyojisung <p990301@gmail.com>
2022-02-14 14:12:38 +0900

    BaekJoon 18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/joon18108.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class joon18108 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(in.readLine()) - 543);
+    }
+}
