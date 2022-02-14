415cbbf181b0077ad0d3e7131c51fe0425108182
pyojisung <p990301@gmail.com>
2022-02-14 14:06:55 +0900

    BaekJoon 10926 ??!

---
+++ src/joon10926.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class joon10926 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(in.readLine() + "??!");
+    }
+}
