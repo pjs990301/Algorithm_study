4cce6635388288a238c2648824ef3fb51812d46b
pyojisung <p990301@gmail.com>
2022-02-16 13:01:35 +0900

    BaekJoon 11866 요세푸스 문제0

---
+++ src/joon11866.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class joon11866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(str.nextToken());
+        int k = Integer.parseInt(str.nextToken());
+        Deque<Integer> que = new LinkedList();
+        String result = "<";
+        for (int i = n; i >= 1; i--) {
+            que.push(i);
+        }
+        while (!que.isEmpty()) {
+            for (int i = 0; i < k - 1; i++) {
+                que.offer(que.poll());
+            }
+            result += que.poll() + ", ";
+        }
+        result = result.substring(0,result.length()-2);
+        result += ">";
+        System.out.println(result);
+    }
+}
