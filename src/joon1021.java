f9b43853763140dcc6c158f2dfa6f44a589a3870
pyojisung <p990301@gmail.com>
2022-02-18 13:56:52 +0900

    BaekJoon 1021 회전하는 큐(다시 풀기)

---
+++ src/joon1021.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class joon1021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(str.nextToken());
+        int m = Integer.parseInt(str.nextToken());
+        LinkedList<Integer> que = new LinkedList();
+        for (int i = 1; i <= n; i++) {
+            que.push(i);
+        }
+
+        int[] seq = new int[m];    // 뽑고자 하는 수를 담은 배열
+
+        str = new StringTokenizer(in.readLine(), " ");
+        for (int i = 0; i < m; i++) {
+            seq[i] = Integer.parseInt(str.nextToken());
+        }
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            int targetIdx = que.indexOf(seq[i]);
+            int halfIdx;
+
+
+            if (que.size() % 2 == 0) {
+                halfIdx = que.size() / 2 - 1;
+            } else {
+                halfIdx = que.size() / 2;
+            }
+            if (targetIdx <= halfIdx) {
+                for (int j = 0; j < targetIdx; j++) {
+                    int temp = que.pollFirst();
+                    que.offerLast(temp);
+                    count++;
+                }
+            } else {
+                for (int j = 0; j < que.size() - targetIdx; j++) {
+                    int temp = que.pollLast();
+                    que.offerFirst(temp);
+                    count++;
+                }
+
+            }
+            que.pollFirst();
+        }
+
+        System.out.println(count);
+
+    }
+
+}
+
