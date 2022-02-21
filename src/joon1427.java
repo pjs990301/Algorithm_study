612565b362960b9d80620715bc7082c38605627b
pyojisung <p990301@gmail.com>
2022-02-21 14:04:41 +0900

    BaekJoon 1427 소트인사이드

--- src/joon1427.java
+++ src/joon1427.java
@@ -9,11 +9,11 @@ public class joon1427 {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String str = in.readLine();
         Integer[] arr = new Integer[str.length()];
-        for(int i = 0; i < str.length();i++){
-            arr[i] = Integer.valueOf((int)str.charAt(i) - '0');
+        for (int i = 0; i < str.length(); i++) {
+            arr[i] = Integer.valueOf((int) str.charAt(i) - '0');
         }
-        Arrays.sort(arr,Collections.reverseOrder());
-        for(int i = 0; i < arr.length;i++){
+        Arrays.sort(arr, Collections.reverseOrder());
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
     }
