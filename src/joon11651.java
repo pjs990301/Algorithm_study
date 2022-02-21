0e2c4a20bcb48768cefdd88e5dc910c16170d545
pyojisung <p990301@gmail.com>
2022-02-21 14:39:47 +0900

    BaekJoon 11651 좌표 정렬하기2

---
+++ src/joon11651.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class joon11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer str = new StringTokenizer(in.readLine());
+            arr[i][0] = Integer.parseInt(str.nextToken());
+            arr[i][1] = Integer.parseInt(str.nextToken());
+        }
+        Arrays.sort(arr, (e1, e2) -> {
+            if (e1[1] == e2[1]) {
+                return e1[0] - e2[0];
+            } else {
+                return e1[1] - e2[1];
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
