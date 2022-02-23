1cee9f705d08548f9ede8f4e111c6d4eea6fd0ff
pyojisung <p990301@gmail.com>
2022-02-23 14:02:25 +0900

    프로그래머스 2021 Dev-Matching: 웹 백엔드 개발자(상반기) 로또의 최고 순위와 최저 순위

--- src/programmers77484.java
+++ src/programmers77484.java
@@ -21,72 +21,71 @@ public class programmers77484 {
         }
         System.out.println(sc.solution(lottos, win_nums));
     }
+}
 
-    static class Solution {
-        public int[] solution(int[] lottos, int[] win_nums) {
-            Arrays.sort(lottos);
-            Arrays.sort(win_nums);
-            ArrayList match = new ArrayList<>();
-            int count = 0;
-            int high = 0;
-            int low = 0;
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        Arrays.sort(lottos);
+        Arrays.sort(win_nums);
+        ArrayList match = new ArrayList<>();
+        int count = 0;
+        int high = 0;
+        int low = 0;
 
-            for (int i = 0; i < 6; i++) {
-                if (lottos[i] == 0)
-                    count++;
-            }
-            for (int i = 0; i < 6; i++) {
-                for (int j = 0; j < 6; j++) {
-                    if (lottos[i] == win_nums[j])
-                        match.add(lottos);
-                }
-            }
-            switch (match.size() + count){
-                case 6:
-                    high = 1;
-                    break;
-                case 5:
-                    high = 2;
-                    break;
-                case 4:
-                    high = 3;
-                    break;
-                case 3:
-                    high = 4;
-                    break;
-                case 2:
-                    high = 5;
-                    break;
-                default:
-                    high = 6;
-                    break;
-            }
-            switch (match.size()){
-                case 6:
-                    low = 1;
-                    break;
-                case 5:
-                    low = 2;
-                    break;
-                case 4:
-                    low = 3;
-                    break;
-                case 3:
-                    low = 4;
-                    break;
-                case 2:
-                    low = 5;
-                    break;
-                default:
-                    low = 6;
-                    break;
-            }
-            int[] answer = {high,low};
-            for (int i = 0; i < answer.length; i++) {
-                System.out.println(answer[i]);
-            }
-            return answer;
+        for (int i = 0; i < 6; i++) {
+            if (lottos[i] == 0)
+                count++;
         }
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (lottos[i] == win_nums[j])
+                    match.add(lottos);
+            }
+        }
+        switch (match.size() + count) {
+            case 6:
+                high = 1;
+                break;
+            case 5:
+                high = 2;
+                break;
+            case 4:
+                high = 3;
+                break;
+            case 3:
+                high = 4;
+                break;
+            case 2:
+                high = 5;
+                break;
+            default:
+                high = 6;
+                break;
+        }
+        switch (match.size()) {
+            case 6:
+                low = 1;
+                break;
+            case 5:
+                low = 2;
+                break;
+            case 4:
+                low = 3;
+                break;
+            case 3:
+                low = 4;
+                break;
+            case 2:
+                low = 5;
+                break;
+            default:
+                low = 6;
+                break;
+        }
+        int[] answer = {high, low};
+        for (int i = 0; i < answer.length; i++) {
+            System.out.println(answer[i]);
+        }
+        return answer;
     }
-
 }
