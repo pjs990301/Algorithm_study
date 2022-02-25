8811646335554e9c505d0178110625733f06c6f4
pyojisung <p990301@gmail.com>
2022-02-25 13:29:42 +0900

    BaekJoon 1541 잃어버린 괄호

---
+++ src/joon1541.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class joon1541 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String input = in.readLine();
+
+        int result = Integer.MAX_VALUE;
+        String[] arr = input.split("-");
+
+        for(int i = 0; i < arr.length; i++) {
+            int temp = 0;
+            String[] add = arr[i].split("\\+");
+
+            for(int j = 0; j < add.length; j++) {
+                temp += Integer.parseInt(add[j]);
+            }
+            if (result == Integer.MAX_VALUE) {
+                result = temp;
+            } else {
+                result -= temp;
+            }
+        }
+        System.out.println(result);
+    }
+}
