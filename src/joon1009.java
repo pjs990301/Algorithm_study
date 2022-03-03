15f7517d327ab170c4ff9825ec63a92063cae7e4
Pio <p990301@gmail.com>
2022-03-03 16:40:13 +0900

    BaekJoon 1009 분산처리

---
+++ src/joon1009.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.lang.management.BufferPoolMXBean;
+
+public class joon1009 {
+    public static void main(String[] args) {
+        BufferedReader in = new BufferedReader(System.in)
+    }
+}
