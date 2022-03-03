3c735baaf266abf0ccd82110710a9a01920d5770
pyojisung <p990301@gmail.com>
2022-03-03 16:50:08 +0900

    BaekJoon 1009 분산처리

--- src/joon1009.java
+++ src/joon1009.java
@@ -1,8 +1,25 @@
 import java.io.BufferedReader;
-import java.lang.management.BufferPoolMXBean;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class joon1009 {
-    public static void main(String[] args) {
-        BufferedReader in = new BufferedReader(System.in)
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(in.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer str = new StringTokenizer(in.readLine());
+            long a = Long.parseLong(str.nextToken());
+            long b = Long.parseLong(str.nextToken());
+            int r = 1;
+            for(int j = 0; j<b;j++){
+                r = (int) ((a * r) %10);
+            }
+            if(r == 0) r = 10;
+            System.out.println(r);
+        }
+
     }
 }
