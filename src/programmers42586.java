a586100a9f06b94c2a52a29744275ddd46d8ab20
Pio <p990301@gmail.com>
2022-03-05 22:48:03 +0900

    프로그래머스 코딩테스트 고득점 Kit 스택/큐 기능개발

---
+++ src/programmers42586.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+public class programmers42586 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution42586 {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] day = new int[progresses.length];
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            day[i] = (int) Math.ceil((double) (100 - progresses[i]) / speeds[i]);
+        }
+
+        stack.push(0);
+        for (int i = 1; i < day.length; i++) {
+            if (day[i] > day[stack.peek()]) {
+                stack.push(i);
+            }
+        }
+
+        int[] answer = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            if (i == stack.size() - 1) {
+                answer[i] = day.length - stack.elementAt(i);
+
+            } else {
+                answer[i] = stack.elementAt(i + 1) - stack.elementAt(i);
+            }
+        }
+        return answer;
+    }
+}
