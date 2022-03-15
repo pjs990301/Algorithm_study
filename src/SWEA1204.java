8112436bb64abf51642b7619eb238c9784be74b4
Pio <p990301@gmail.com>
2022-03-15 21:52:05 +0900

    SWExpertAcademy [S/W 문제해결 기본] 1일차 - 최빈수 구하기

---
+++ src/SWEA1204.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SWEA1204 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int scores[] = new int[1000];
+            int count[] = new int[101];
+            int maxScore = 0;
+
+            int tc = sc.nextInt();
+
+            for (int j = 0; j < 1000; j++) {
+                scores[j] = sc.nextInt();
+                count[scores[j]]++;
+            }
+            for (int j = 0; j < count.length - 1; j++) {
+                if (count[maxScore] <= count[j]) {
+                    maxScore = j;
+                }
+            }
+            System.out.println("#" + tc + " " + maxScore);
+        }
+    }
+}
