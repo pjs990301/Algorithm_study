1abdb485ec82a84b58ea4f78c193685a37a1c2f0
Pio <p990301@gmail.com>
2022-03-15 21:54:44 +0900

    SWExpertAcademy 1204. [S/W 문제해결 기본] 1일차 - 최빈수 구하기

--- src/SWEA2072.java
+++ src/SWEA2072.java
@@ -4,16 +4,15 @@ public class SWEA2072 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T;
-        T=sc.nextInt();
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
+        T = sc.nextInt();
+        for (int test_case = 1; test_case <= T; test_case++) {
             int sum = 0;
             for (int j = 0; j < 10; j++) {
                 int n = sc.nextInt();
                 if (n % 2 == 1)
                     sum += n;
             }
-            System.out.print("#"+ test_case + " ");
+            System.out.print("#" + test_case + " ");
             System.out.println(sum);
         }
     }
