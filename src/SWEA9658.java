ee98f4424b3b737cef4b6c0c1169eff8518cb071
Pio <p990301@gmail.com>
2022-03-15 22:25:05 +0900

    SWExpertAcademy 9658. 유효숫자 표기

--- src/SWEA9658.java
+++ src/SWEA9658.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class SWEA9658 {
     public static void main(String[] args) throws IOException {
