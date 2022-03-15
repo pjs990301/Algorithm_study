6571e34c49198875d2a9cab1235bbb22fe47dcf5
Pio <p990301@gmail.com>
2022-03-15 22:16:42 +0900

    SWExpertAcademy 2071. 평균값 구하기

---
+++ src/SWEA2071.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.util.Arrays.stream;
+
+public class SWEA2071 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+        //Case 1) Stream을 이용
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int[] arr = new int[10];
+            for(int i = 0; i < 10; i++){
+                arr[i] = sc.nextInt();
+            }
+            System.out.println("#" + test_case + " " + Math.round((stream(arr).average().getAsDouble())));
+
+        }
+        System.out.println();
+        //Case 2) //Sum을 이용
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int sum = 0;
+            for(int i = 0; i < 10; i++){
+                sum += sc.nextInt();
+            }
+            System.out.format( "#%d %.0f\n", test_case, sum/10.0);
+        }
+    }
+}
