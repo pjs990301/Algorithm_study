0786a36e320640a03020bb4e50d883cb37365d09
Pio <p990301@gmail.com>
2022-04-04 00:44:54 +0900

    SWExpertAcademy 2047. 신문 헤드라인

---
+++ src/SWEA/SWEA2047.java
@@ -0,0 +1,12 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class SWEA2047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        str = str.toUpperCase();
+        System.out.println(str);
+    }
+}
