6b4fab28ae7d8fc526e8fcc8a4e8802ec7fdd8c6
Pio <p990301@gmail.com>
2022-04-04 01:01:35 +0900

    SWExpertAcademy 2029. 몫과 나머지 출력하기

---
+++ src/SWEA/SWEA2029.java
@@ -0,0 +1,16 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class SWEA2029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("#" + test_case + " " + a / b + " " + a % b);
+        }
+    }
+}
