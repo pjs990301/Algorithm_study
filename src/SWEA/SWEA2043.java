ec26b2f3321607644fd4684241b13e63907dd7d9
Pio <p990301@gmail.com>
2022-04-04 00:54:54 +0900

    SWExpertAcademy 2043. 서랍의 비밀번호

---
+++ src/SWEA/SWEA2043.java
@@ -0,0 +1,12 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class SWEA2043 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a - b + 1);
+    }
+}
