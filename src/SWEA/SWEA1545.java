9fb1c1d3aa809bc45a1e8a52d1328449452919cb
Pio <p990301@gmail.com>
2022-04-05 00:44:25 +0900

    SWExpertAcademy 1545. 거꾸로 출력해 보아요

---
+++ src/SWEA/SWEA1545.java
@@ -0,0 +1,16 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class SWEA1545 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = n;
+        while(i!=0){
+            System.out.print(i + " ");
+            i--;
+        }
+        System.out.print("0");
+    }
+}
