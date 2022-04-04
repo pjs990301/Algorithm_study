839e4b8d133eb9b89ceb34b7f834d105a4303b4f
Pio <p990301@gmail.com>
2022-04-05 00:23:57 +0900

    SWExpertAcademy 2025. N줄덧셈

---
+++ src/SWEA/SWEA2025.java
@@ -0,0 +1,15 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class SWEA2025 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+            sum = sum + i;
+        System.out.println(sum);
+
+    }
+}
