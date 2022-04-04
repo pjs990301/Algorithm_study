5231e51a199b865a2d028e215b7c0390d3c52a59
Pio <p990301@gmail.com>
2022-04-05 00:46:27 +0900

    SWExpertAcademy 2056. 연월일 달력

---
+++ src/SWEA/SWEA2056.java
@@ -0,0 +1,23 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class SWEA2056 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] days= {31,28,31,30,31,30,31,31,30,31,30,31};
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++) {
+            String s=sc.next();
+            String year=s.substring(0,4);
+            String month=s.substring(4,6);
+            String day=s.substring(6,8);
+            if(Integer.parseInt(month)>=1 && Integer.parseInt(month)<=12 && Integer.parseInt(day)>=0
+                    && Integer.parseInt(day)<=days[Integer.parseInt(month)-1])
+                System.out.println("#"+(i+1)+" "+year+"/"+month+"/"+day);
+            else {
+                System.out.println("#"+(i+1)+" -1");
+            }
+        }
+    }
+}
