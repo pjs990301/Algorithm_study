465f12bc253444c9092f216c3d0a54b5360f5ead
Pio <p990301@gmail.com>
2022-04-05 00:25:57 +0900

    SWExpertAcademy 1938. 아주 간단한 계산기

---
+++ src/SWEA/SWEA1938.java
@@ -0,0 +1,16 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class SWEA1938 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+    }
+}
