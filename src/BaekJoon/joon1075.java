93831e3364aec5e71c6043fb96be94a3ef583388
Pio <p990301@gmail.com>
2022-06-25 21:49:05 +0900

    BaekJoon 1075 나누기

--- src/BaekJoon/joon1075.java
+++ src/BaekJoon/joon1075.java
@@ -1,4 +1,24 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class joon1075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int f = Integer.parseInt(br.readLine());
+
+        int temp = (n / 100) * 100;
+        while (true) {
+            if (temp % f == 0) {
+                int result = temp % 100;
+                if (result < 10) System.out.println("0" + result);
+                else System.out.println(result);
+                return;
+            }
+            temp++;
+        }
+    }
 }
